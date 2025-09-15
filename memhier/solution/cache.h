03b3d981b5bee84ccc2946df3ad7b74255cdd108
Steven Dao <sdao@vols.utk.edu>
2025-09-15 15:06:40 -0400

    updated the cache.h with "forward facing" calls that are meant to be used between caches or by the user

--- memhier/solution/cache.h
+++ memhier/solution/cache.h
@@ -1,8 +1,14 @@
 #include <stddef.h>
+#include <stdint.h>
 
 typedef struct Cache Cache;
 
 Cache* cache_new(const size_t num_sets, const size_t set_size, const size_t line_size, const bool write);
-void cache_invalidate(Cache* cache);
+
+void cache_write(Cache* cache, const uint32_t address);
+void cache_read(Cache* cache, const uint32_t address);
+void cache_invalidate_all(Cache* cache);
+void cache_invalidate_entry(Cache* cache, const uint32_t address);
 void cache_free(Cache* cache);
+
 void cache_decode_debug(const Cache* cache, const char* cache_name);
